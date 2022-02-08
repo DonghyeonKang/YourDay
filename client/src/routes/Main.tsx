7d4d63ff63952a852e5dd0dda2695c5eee4c345b
Donghyeon Kang <86303312+DonghyeonKang@users.noreply.github.com>
2022-02-08 16:34:24 +0900

    Add Chart

--- client/src/routes/Main.tsx
+++ client/src/routes/Main.tsx
@@ -2,22 +2,19 @@ import React from "react";
 import "./css/Main.css";
 
 function Main() {
-    return (
-        <div className="Main">
-            
-            <div className="mainImage">
-                <img className="backgroundImage" alt="sky" src="img/sky.png" />
-            </div>
-            
-            <div className = "content"> 
-                <h1>현재 시간: </h1>
-                <h1>남은 시간: </h1>
-                <h1>낭비한 시간: </h1>
-            </div>
-            
+  return (
+    <div className="Main">
+      <div className="mainImage">
+        <img className="backgroundImage" alt="sky" src="img/sky.png" />
+      </div>
 
-        </div>
-    )
+      <div className="content">
+        <h1>현재 시간: </h1>
+        <h1>남은 시간: </h1>
+        <h1>낭비한 시간: </h1>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
