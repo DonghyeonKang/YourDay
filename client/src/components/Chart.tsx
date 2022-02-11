26d0d625080e526ef64ca99b2780886f8ced5f23
Donghyeon Kang <86303312+DonghyeonKang@users.noreply.github.com>
2022-02-11 15:21:59 +0900

    Update chart component

--- client/src/components/Chart.tsx
+++ client/src/components/Chart.tsx
@@ -1,67 +1,39 @@
 import React from "react";
 import "./Chart.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Pie, Bar } from "react-chartjs-2";
-import { MDBContainer } from "mdbreact";
-import { Chart, ArcElement, CategoryScale, registerables } from "chart.js";
-Chart.register(ArcElement);
-// chart 에서 받을 데이터 chartmode(pie, bar), pie_data(수행한 시간, 낭비한 시간, 미등록 시간), date(현재 날짜)
-// bar_data(week{x_element: 월화수목금토일}, month{x_element:1,2,3, 4} (주차), year{x_element: 1,2,3 .. 12})
+import BarChart from "./BarChart";
+import PieChart from "./PieChart";
 
-const chartName = "월";
-const pie_data = {
-  saveTime: 16,
-  wasteTime: 7,
-  Unregistered: 1,
-};
-const date = {
-  year: 2022,
-  month: 2,
-  day: 8,
-};
-const bar_data = {
-  y_element: [20, 40, 70, 0, 0, 0, 0],
-};
-const month_data = {
-  y_element: [20, 70, 20, 40], //4 - 5개
-};
-const year_data = {
-  y_element: [20, 70, 20, 40, 20, 70, 20, 40, 0, 0, 0, 0], //12개
-};
-const color_data = {
-  backGroundColor: ["#46BFBD", "#4D5360", "#AC64AD"],
-  hoverBackgoundColor: ["#5AD3D1", "#616774", "#DA92DB"],
-};
+interface propTypes {
+  chartType: number;
+  setChartType: (num: number) => void;
+  chartName: string;
+  data: number[];
+}
 
-class ChartsPagePie extends React.Component {
-  state = {
-    dataPie: {
-      labels: ["수행한 시간", "미등록 시간", "낭비한 시간"],
-      datasets: [
-        {
-          data: [300, 50, 120],
-          backgroundColor: ["#46BFBD", "#4D5360", "#AC64AD"],
-          hoverBackgroundColor: ["#5AD3D1", "#616774", "#DA92DB"],
-        },
-      ],
-    },
-  };
-
-  render() {
+const chart = (props: propTypes) => {
+  let chartMode = 0;
+  
+  if (chartMode) {
     return (
       <>
         <div className="main_section_inner_content">
-          <h2 className="content_title">{chartName}</h2>
+          <h2 className="content_title">{props.chartName}</h2>
           <div className="content_inner">
-            <MDBContainer>
-              <Pie data={this.state.dataPie} options={{ responsive: true }} />
-            </MDBContainer>
+            <PieChart data={props.data} />
             <div className="explain_color">
               <p>수행한 시간</p>
               <p>낭비한 시간</p>
               <p>미등록 시간</p>
             </div>
           </div>
+          <aside className="menu">
+            <a className="menu_saveTime" onClick={() => (chartMode = 0)}>
+              수행한 시간
+            </a>
+            <a className="menu_wasteTime" onClick={() => (chartMode = 1)}>
+              낭비한 시간
+            </a>
+          </aside>
           <div className="content_comment">
             <h3>Comment</h3>
             <p>오늘 완수한 시간은 총 12시간입니다!</p>
@@ -69,67 +41,27 @@ class ChartsPagePie extends React.Component {
         </div>
       </>
     );
-  }
-}
-
-Chart.register(CategoryScale);
-Chart.register(...registerables);
-class ChartsPageBar extends React.Component {
-  state = {
-    dataBar: {
-      labels: ["월", "화", "수", "목", "금", "토", "일"],
-      datasets: [
-        {
-          label: '월별 평균 수행시간',
-          data: [12, 19, 3, 5, 2, 3, 12],
-          backgroundColor: [
-            "rgba(255, 134,159,0.4)",
-            "rgba(98,  182, 239,0.4)",
-            "rgba(255, 218, 128,0.4)",
-            "rgba(113, 205, 205,0.4)",
-            "rgba(170, 128, 252,0.4)",
-            "rgba(255, 177, 101,0.4)",
-            "rgba(230, 157, 201,0.4)",
-          ],
-          borderWidth: 2,
-          borderColor: [
-            "rgba(255, 134, 159, 1)",
-            "rgba(98,  182, 239, 1)",
-            "rgba(255, 218, 128, 1)",
-            "rgba(113, 205, 205, 1)",
-            "rgba(170, 128, 252, 1)",
-            "rgba(255, 177, 101, 1)",
-            "rgba(230, 157, 201, 1)",
-          ],
-        },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          ticks: {
-            color: 'white'
-          },
-        },
-        x: {
-          ticks: {
-            color: 'white'
-          },
-        }
-      }
-    }
-  };
-
-  render() {
+  } else {
     return (
       <>
         <div className="main_section_inner_content">
-          <h2 className="content_title">{chartName}</h2>
+          <h2 className="content_title">{props.chartName}</h2>
           <div className="content_inner">
-            <MDBContainer>
-              <Bar data={this.state.dataBar} options={this.state.options} />
-            </MDBContainer>
+            <BarChart data={props.data} chartType={props.chartType}/>
+            <div className="explain_color">
+              <p>수행한 시간</p>
+              <p>낭비한 시간</p>
+              <p>미등록 시간</p>
+            </div>
           </div>
+          <aside className="menu">
+            <a className="menu_saveTime" onClick={() => (chartMode = 0)}>
+              수행한 시간
+            </a>
+            <a className="menu_wasteTime" onClick={() => (chartMode = 1)}>
+              낭비한 시간
+            </a>
+          </aside>
           <div className="content_comment">
             <h3>Comment</h3>
             <p>오늘 완수한 시간은 총 12시간입니다!</p>
@@ -138,6 +70,6 @@ class ChartsPageBar extends React.Component {
       </>
     );
   }
-}
+};
 
-export default ChartsPageBar;
+export default chart;
--- client/src/routes/MyDays.tsx
+++ client/src/routes/MyDays.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/MyDays.css";
 import Header from "../components/Header";
 import Chart from "../components/Chart";
 
+const chartName = "월";
+
+const getChartData = (chartType: number) => {
+  //api 요청해서 데이터 받아야함
+  if (chartType == 0) {
+    const y_element = [20, 40, 70, 0, 0, 0, 0];
+    return y_element;
+  } else if (chartType == 1) {
+    const y_element = [20, 70, 20, 40];
+    return y_element;
+  } else if (chartType == 2) {
+    const y_element = [20, 70, 20, 40, 20, 70, 20, 40, 0, 0, 0, 0];
+    return y_element;
+  } else {
+    const pieData = [16, 7, 1]      // saveTime: 16, wasteTime: 7, Unregistered: 1,
+    return pieData;
+  }
+};
+
 function MyDays() {
+  const [chartType, setChartType] = useState(0);
+  const [data, setData] = useState(getChartData(0));
+
   return (
     <>
       <Header />
@@ -11,35 +33,55 @@ function MyDays() {
         <section className="main_section">
           <h2 className="main_section_header">튼튼소열이님의 하루는,</h2>
           <div className="main_section_inner">
-            <aside className="menu">
-              <a className="menu_saveTime" href="#">
-                수행한 시간
-              </a>
-              <a className="menu_wasteTime" href="#">
-                낭비한 시간
-              </a>
-            </aside>
-            <Chart />
             <aside className="period_nav">
-              <a className="day" href="#">
+              <a
+                className="day"
+                onClick={() => {
+                  setChartType(0);
+                  setData(getChartData(0));
+                }}
+              >
                 일
               </a>
-              <a className="week" href="/frontend/public/myDays/saveTime_week.html">
+              <a
+                className="week"
+                onClick={() => {
+                  setChartType(1);
+                  setData(getChartData(0));
+                }}
+              >
                 주
               </a>
-              <a className="month" href="#">
+              <a
+                className="month"
+                onClick={() => {
+                  setChartType(2);
+                  setData(getChartData(0));
+                }}
+              >
                 월
               </a>
-              <a className="year" href="#">
+              <a
+                className="year"
+                onClick={() => {
+                  setChartType(3);
+                  setData(getChartData(0));
+                }}
+              >
                 년
               </a>
             </aside>
+            <Chart
+              chartType={chartType}
+              setChartType={setChartType}
+              chartName={chartName}
+              data={data}
+            />
           </div>
         </section>
       </div>
     </>
   );
 }
-// api 콜 후 state로 데이터 넣기 - chart, comment
 
 export default MyDays;
