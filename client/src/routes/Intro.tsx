21eeee732765d8ce978197e6f95ef7718c147a3f
DonghyeonKang <donghyeon3372@gmail.com>
2022-03-01 16:01:05 +0900

    Add Intro

--- client/src/routes/Intro.tsx
+++ client/src/routes/Intro.tsx
@@ -1,10 +1,40 @@
 import React from "react";
-
 import "./css/Intro.css";
+import introimg from "../assets/intro.png";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-export default function Login() {
+const StyledDiv = styled.div`
+  & {
+    background-image: url("./intro.png") !important;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background-size: cover;
+    background-position: 50% 50%;
+    text-align: center;
+  }
+  &:before {
+    opacity: .5;
+    content: "";
+    z-index: 100;
+  }
+`;
+
+const turnMain = () => {
+
+}
+
+export default function Intro() {
   return (
-    <div className="container">
-   </div>
+    <StyledDiv>
+      <div className="title">
+        <div className="main">너의 하루는,</div>
+        <div className="sub">당신은 빛나는 하루를 보내시고 계신가요?</div>
+      </div>
+      <Link className="start_button" onClick={turnMain} to="/time">
+        <span>시작하기</span>
+      </Link>
+    </StyledDiv>
   );
 }
