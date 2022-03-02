15c33c9f36df414e106c23ed10c2cb0cf602c81f
정윤수 <78465062+dbstn1325@users.noreply.github.com>
2022-03-02 11:24:53 +0900

    Correct server Error

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 // import { typeORMConfig } from './configs/typeorm.config';
-import { AuthModule } from './auth/auth.module';
 import { FriendsModule } from './friends/friends.module';
 import { join } from 'path/posix';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-<<<<<<< HEAD
       type: 'mysql',
       host: 'localhost',
       port: 3306,
@@ -17,19 +15,8 @@ import { join } from 'path/posix';
       database: 'user_db',
       entities: [join(__dirname, '**', '*.entity.{ts,js}')],
       synchronize: true,
-=======
-      "type": "mysql",
-      "host": "localhost",
-      "port": 3306,
-      "username": "root",
-      "password": "gold3819",
-      "database": "user_db",
-      "entities": ["dist/**/*.entity{.ts,.js}"],
-      "synchronize": true
->>>>>>> b46d4a9ac3e5d03a8dfeb08cee7c0b501ebb85f5
     }),
     UsersModule,
-    AuthModule,
     FriendsModule,
   ],
 })
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
-import { Body, ConsoleLogger, Controller, Delete, Get, Header, Param, ParseIntPipe, Post, Req, Request, Res, Response } from '@nestjs/common';
-=======
+
+import { ConsoleLogger, Header, Request, Res, Response } from '@nestjs/common';
+
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, UseGuards,Req } from '@nestjs/common';
->>>>>>> b46d4a9ac3e5d03a8dfeb08cee7c0b501ebb85f5
+
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 import { CreateUserDto, SearchUserDto } from './dto/create-user.dto';
-import { AuthService } from 'src/auth/auth.service';
 import { map, Observable } from 'rxjs';
 import { AxiosResponse } from "axios";
 
@@ -19,8 +18,7 @@ import { AuthGuard } from '@nestjs/passport';
 export class UsersController {
     constructor(private usersService: UsersService) {}
     
-<<<<<<< HEAD
-    
+
     @Get('/')
     getUserName(): Promise<any> {
         return this.usersService.getUserName();
@@ -34,30 +32,13 @@ export class UsersController {
     @Get('/friendList')
     getAllUsers(): Promise<any> {
         return this.usersService.getAllUser();
-=======
-    @Get('/google')
-    @UseGuards(AuthGuard('google'))
-    async googleAuth(@Req() req) {
->>>>>>> b46d4a9ac3e5d03a8dfeb08cee7c0b501ebb85f5
     }
-
-    @Get('auth/google/callback')
-    @UseGuards(AuthGuard('google'))
-    googleAuthredirect(@Req() req){
-        return this.usersService.googleLogin(req)
-    }
-
-    // @Get('/')
-    // getAllUsers(): Promise<any> {
-    //     return this.usersService.getAllUser();
-    // }
 
     //편집(삭제, 찾을때)
     @Delete('/friendList/edit/:id')
     deleteUser(@Param('id', ParseIntPipe) id:number): Promise<void> {
         return this.usersService.deleteUser(id);
     }
-
     
     //윤수 찾을 때,
     @Get('/friendList/edit/:name')
@@ -91,7 +72,8 @@ export class UsersController {
     }
 
    
-    
+}
 
     
-}
+    
+
