6c3d68322fdd243e2c811ab34b333e44443bf8f8
DonghyeonKang <donghyeon3372@gmail.com>
2022-03-12 20:27:18 +0900

    Add dayChart

--- server/src/schedule/schedule.module.ts
+++ server/src/schedule/schedule.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleRepository } from './schedule.repository'
 import { ScheduleService } from './schedule.service';
+import { ScheduleController } from './schedule.controller';
 
 @Module({
     imports: [
       TypeOrmModule.forFeature([ScheduleRepository])
     ],
     providers: [ScheduleService],
-    exports: [ScheduleService]  // cron service로 보내기 위함
+    exports: [ScheduleService],
+    controllers: [ScheduleController]  // cron service로 보내기 위함
   })
 export class scheduleModule {}
   
--- server/src/schedule/schedule.service.ts
+++ server/src/schedule/schedule.service.ts
@@ -15,6 +15,31 @@ export class ScheduleService {
         return data;
     }
 
+    async getTime(id) {
+        const data = await this.scheduleRepository.find({ user: id });
+        let saveTime = 0;
+        
+        // 수행한 시간 계산
+        let noneTime = 24;
+        for (let i = 0; i < data.length; i++) {
+          const start = new Date(data[i].start_time);
+          const end = new Date(data[i].end_time);
+          // 오차 수정
+          start.setTime(
+            start.getTime() - new Date().getTimezoneOffset() * 60 * 1000,
+            );
+          end.setTime(end.getTime() - new Date().getTimezoneOffset() * 60 * 1000);      
+          // 시간 차이 계산
+          const diffTime = (end.getTime() - start.getTime()) / (1000 * 60 * 60);
+          noneTime -= diffTime;
+          saveTime += (diffTime / 100) * data[i].percent;      
+        }
+        saveTime = parseInt(saveTime.toFixed(1));
+        const wasteTime = 24 - (saveTime + noneTime);
+        const timeData = [saveTime, wasteTime, noneTime];
+        return timeData;
+      }
+
     // 데이터를 모두 정리하면, 기존의 스케쥴은 초기화 한다. 
     async initializeTable() {
         await this.scheduleRepository.clear(); 
